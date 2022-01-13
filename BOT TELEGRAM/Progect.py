09f8724024b24fda814e99b3125228319f872e1f
EvDuch <79639567+EvDuch@users.noreply.github.com>
2022-01-13 17:25:00 +0700

    Update Progect.py

--- BOT TELEGRAM/Progect.py
+++ BOT TELEGRAM/Progect.py
@@ -1,13 +1,13 @@
 #TELEGRAM BOT - МОЖЕТ ВЫВЕСТИ ПОГОДУ В ЛЮБОМ ГОРОДЕ. ПРОСТО НАПИШИТЕ НАЗВАНИЕ ГОРОДА ЕМУ В ЛИЧНОЕ СООБЩЕНЕ (Для начала работы введите команду /start)
 
-from pyowm import OWM
+from pyowm import OWM #МОДУЛ ДЛЯ РАБОТЫ С ПОГОДОЙ
 from translate import Translator
 import telebot #МОДУЛЬ ДЛЯ СОЗДАНИЯ БОТА В TELEGRAM
 import config  #СОБСТВ. МОДУЛЬ С НЕЗНАЧИТЕЛЬНЫМИ ПЕРЕМЕННЫМИ
 
 translator = Translator(to_lang="Russian") # изначально данные о погоде выводятся на английском, поэтому пришлось переводить с помощью данной строки
 
-owm = OWM('7f0fb6e211ca80b8042a2197c3030206') #работа с погодой
+owm = OWM('7f0fb6e211ca80b8042a2197c3030206') #работа с погодой (ВВОДИМ APY-KEY)
 mgr = owm.weather_manager()
 bot = telebot.TeleBot(config.TOKEN) #ПРИСВАИВАНИЕ ТЕГА БОТА
 
@@ -16,11 +16,16 @@ def welcome(message):
     bot.send_message(message.chat.id, 'Здравствуйте! Вас приветствует weather_telegrambot. Введите надвание города в котором хотите узнать погоду.')
 
 @bot.message_handler(content_types=["text"]) #ХЕНДЛЕР КОМАНДЫ
-def send_welcome(message):
-    place = message.text
+def send_welcome(message):              #функция принимает сообщение Боту как название города и выполняет работу с температурой нашего города
+    place = message.text                #присваиваем переменной название города
+    
+    #НАХОДИМ ТЕКУЩУЮ ПОГОДУ В НАШЕМ ГОРОДЕ:
     observation = mgr.weather_at_place(place)
     w = observation.weather
+    
+    #С ПОМОЩЬЮ МОДУЛЯ TRANSLATOR ПЕРЕВОДИМ ПОЛУЧЕННУЮ ИНФОРМАЦИЮ (Т К ИЗНАЧАЛЬНО ОНА ДАНА НА АНГЛИЙСКОМ)
     translation_status = translator.translate(w.detailed_status)#Перевод статуса города на Рус.Яз.
+    
     temperature = str(w.temperature('celsius'))#Присваиваем строку модуля с температурами
     schet = int(temperature.find(","))#Находим первую запятую в этой строке 
     restemp = str(temperature[8:schet])#результат температуры
